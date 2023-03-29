851ae5157bd3cafd6b9c21b5911c959a3ed99dfd
sooyeon-dev <dus3062@daum.net>
2023-03-29 21:51:04 +0900

    리액트 공부

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button.js";
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all that time");
+
+  const onChange = (event) => setKeyword(event.target.value);
+  
+  const iRunOnlyOnece = () => {
+    console.log("i run only onece");
+  };
+  useEffect(iRunOnlyOnece, []);
+
+  useEffect(() => {
+    console.log("Call the API..");
+  }, []);
+
+  useEffect(() => {
+    if(keyword !== "" && keyword.length> 5) {
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input 
+        value={keyword}
+        onChange={onChange} 
+        type="text" 
+        placeholder="Seach here">
+      </input>
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>Click Me</button>
     </div>
   );
 }
