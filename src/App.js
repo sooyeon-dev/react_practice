e5623be6cfe292546d3518141782d1619b8913c6
sooyeon-dev <dus3062@daum.net>
2023-04-03 22:57:11 +0900

    React-router

--- src/App.js
+++ src/App.js
@@ -1,58 +1,24 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async() => {
-    const response = await fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-    )
-
-    const json = await response.json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getMovies()
-  }, []);
-
-  // fetch 대신 요즘은 async-await 사용
-  // useEffect(() => {
-    // fetch(
-    //   `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-    // )
-    //   .then((response) => response.json())
-    //   .then((json) => {
-    //     setMovies(json.data.movies);
-    //     setLoading(false);
-    //   });
-  // }, []);
-
   return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1> 
-      ) : (
-        <div>
-          {movies.map(movie => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image}></img>
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                <li>
-                  {movie.genres.map(g => (
-                    <li key={g}>{g}</li>
-                  ))}
-                </li>
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
+    <Router>
+        <Switch> {/* Switch 사용 이유 : 한번에 하나의 Route만 렌더링 하기 위해 */}
+            <Route path="/movie">
+              <Detail />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+        </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/Movie.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import { Link } from "react-router-dom";
+
+function Movie({coverImg, title, summary, genres}) {
+    return (
+        <div>
+            <img src={coverImg} alter={title}></img>
+            <h2>
+                <Link to="/movie">{title}</Link>
+            </h2>
+            <p>{summary}</p>
+            <ul>
+            <li>
+                {genres.map(g => (
+                    <li key={g}>{g}</li>
+                ))}
+            </li>
+            </ul>
+        </div>
+    );
+}
+
+Movie.propTypes = {
+    coverImg: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+}
+
+export default Movie;
